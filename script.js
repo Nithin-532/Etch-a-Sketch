0c0958c4b1794e7b5c1c17b81847dc64404b757b
Nithin-532 <kallurunithin8771@gmail.com>
2023-10-17 23:46:52 +0530

    added all functionality of random color need to update more

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const grid = document.querySelector('.grid');
 
-function makeDivs(n) {
+function makeDivs(e) {
+  const n = e.target.value;
+  while (grid.firstChild) {
+    grid.removeChild(grid.firstChild);
+  }
   for (let i = 0; i < n; i++) {
     const divRow = document.createElement('div');
     for (let j = 0; j < n; j++) {
@@ -11,16 +15,33 @@ function makeDivs(n) {
     divRow.classList.add('row');
     grid.appendChild(divRow);
   }
+  sketch();
 }
 
-makeDivs(16);
+const input = document.querySelector('#input');
+input.addEventListener('change', makeDivs);
 
-function handleMouseOver(e) {
-  if (e.target.className == 'column') {
-    const column = e.target;
-    column.style.backgroundColor = 'black';
-    e.stopPropagation();
-  }
+// const eraser = document.querySelector('.eraser');
+
+function random(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-grid.addEventListener('mouseover', handleMouseOver);
+function colorCol(e) {
+  const r = random(0, 255);
+  const g = random(0, 255);
+  const b = random(0, 255);
+  const div = e.target;
+  div.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+}
+
+function sketch() {
+  const column = document.querySelectorAll('.column');
+  column.forEach(col => {
+    col.addEventListener('mouseover', colorCol);
+  })
+}
+
+
